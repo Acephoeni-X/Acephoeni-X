3df302be448fee029a1d5268bd25c1b1f1906eb4
Rishi Sharma <rishisharma@Rishis-MacBook-Air.local>
2021-09-19 15:53:10 +0530

    update

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-import requests, json
+import requests, json, urllib.request
 
 class generateGraph():
     __url = "https://api.github.com/users/Rishi-Sharma2002/repos"
 
-    def getData(self):
-        data = requests.get(self.__url).json()
-        myLang = {
-            "Python":0,
-            "C++":0,
-            "JavaScript":0,
-            "Vim script":0,
-            "C":0,
-            "Jupyter Notebook": 0
-        }
+    def getData(self, url = __url):
+        print(urllib.request.getproxies())
+        data = requests.get(url, proxies=urllib.request.getproxies()).json()
+        return data
+
+    def getContributors(self):
+        data = self.getData(self.__url)
+        json_data = []
         for i in data:
-            for j in list(myLang.keys()):
-                if i['language'] == j:
-                    myLang[j]+=1
-        
-        return myLang
+            cont = self.getData(i['contributors_url'])
+            summ = 0
+
+            print(cont)
+            # for j in range(len(cont)):
+            #     summ += cont[j]['contributions']
+            #     print(summ)
+
+        #     json_ = {
+        #                 "name": i['name'],
+        #                 "contributions": summ
+        #             }
+        #     json_data.append(json_)
+        # return json_data
 
 graph = generateGraph()
-myLang = graph.getData()
-lang = list(myLang.keys())
-values = list(myLang.values())
-print(graph.getData())
-# plt.rcParams.update({
-#     "lines.color": "white",
-#     "patch.edgecolor": "white",
-#     "text.color": "black",
-#     "axes.facecolor": "white",
-#     "axes.edgecolor": "lightgray",
-#     "axes.labelcolor": "white",
-#     "xtick.color": "white",
-#     "ytick.color": "white",
-#     "grid.color": "lightgray",
-#     "figure.facecolor": "black",
-#     "figure.edgecolor": "black",
-#     "savefig.facecolor": "black",
-#     "savefig.edgecolor": "black"
-#     })
+myLang = graph.getContributors()
+print(json.dumps(myLang, indent=4))
 
-# plt.bar(lang, values)
-# plt.show()
